818d583fe52d441dcedc956bd8d4e1894d3c396e
dosya <vicdosya@gmail.com>
2022-11-22 13:18:40 +0200

    Server response to client setup

--- server/express.js
+++ server/express.js
@@ -1,4 +1,5 @@
-//Import packages (esm).
+//Import packages (ESM is installed)
+const apiRoutes = require("./api/post-page");
 import express from "express";
 import bodyParser from "body-parser";
 
@@ -8,11 +9,10 @@ const PORT = process.env.PORT || 3001;
 
 //Middleware
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true}));
-
-//Routes
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use("/api", apiRoutes);
 
 //Server Start
 app.listen(PORT, () => {
-    console.log(`Server is listening on port ${PORT}`);
+  console.log(`Server is listening on port ${PORT}`);
 });
