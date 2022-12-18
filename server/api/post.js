11c02d57319f6a6e73a51d108501991c7a80c5dd
dosya <vicdosya@gmail.com>
2022-12-18 11:14:09 +0200

    Add user auth on each api request

--- server/api/post.js
+++ server/api/post.js
@@ -1,47 +1,62 @@
 import express from "express";
 import Post from "../Schemas/Post";
 import Comment from "../Schemas/Comment";
-import UserDetails from "../Schemas/UserDetails";
 import PostVote from "../Schemas/PostVote";
 const app = express();
 
 //Send back all the posts
 app.get("/", async (req, res) => {
-  const posts = await Post.find();
-  res.send(posts);
+  try {
+    const posts = await Post.find();
+    res.send(posts);
+  } catch (err) {
+    res.send({ error: "Something went wrong." });
+    console.log(err);
+  }
 });
 
 //Submitting a post
 app.post("/", async (req, res) => {
-  console.log(req.session.user);
-  const post = new Post({
-    userId: req.session.user._id,
-    title: req.body.title,
-    body: req.body.body,
-    author: req.session.user.fname + " " + req.session.user.lname,
-  });
-  await post.save(); //Updates the document in the DB.
-  res.send({ statusMsg: "Submitted!" });
+  if (!req.session.user) {
+    return res.send({ error: "You are not logged in." });
+  } else {
+    console.log(req.session.user);
+    const post = new Post({
+      userId: req.session.user._id,
+      title: req.body.title,
+      body: req.body.body,
+      author: req.session.user.fname + " " + req.session.user.lname,
+    });
+    await post.save(); //Updates the document in the DB.
+    res.send({ statusMsg: "Submitted!" });
+  }
 });
 
 //Send back individual post
 app.get("/:postId", async (req, res) => {
-  try {
-    const post = await Post.findById(req.params.postId);
-    if (!post) {
-      return res.send({ error: "Post not found." });
+  if (!req.session.user) {
+    return res.send({ error: "You are not logged in." });
+  } else {
+    try {
+      const post = await Post.findById(req.params.postId);
+      if (!post) {
+        return res.send({ error: "Post not found." });
+      }
+      res.send(post);
+    } catch (err) {
+      res.send({ error: "Invalid id" });
     }
-    res.send(post);
-  } catch (err) {
-    res.send({ error: "Invalid id" });
   }
 });
 
 //Submitting a comment to individual posts
 app.post("/:postId/comment", async (req, res) => {
+  if (!req.session.user) {
+    return res.send({ error: "You are not logged in." });
+  }
   const post = await Post.findById(req.params.postId);
   if (!post) {
-    res.send({ error: "Invalid post" });
+    return res.send({ error: "Invalid post" });
   } else {
     const comment = new Comment({
       post: req.params.postId,
@@ -59,91 +74,111 @@ app.post("/:postId/comment", async (req, res) => {
 
 //Get the comments of the individual post
 app.get("/:postId/comments", async (req, res) => {
-  try {
-    const comments = await Comment.find({
-      post: req.params.postId,
-    });
-    res.send({ comments });
-  } catch (err) {
-    res.send({ error: "Invalid id" });
+  if (!req.session.user) {
+    return res.send({ error: "You are not logged in." });
+  } else {
+    try {
+      const comments = await Comment.find({
+        post: req.params.postId,
+      });
+      res.send({ comments });
+    } catch (err) {
+      res.send({ error: "Invalid id" });
+    }
   }
 });
 
 //Get all amount of comments of the individual post
 app.get("/:postId/comments/all", async (req, res) => {
-  const post = await Post.find({
-    _id: req.params.postId,
-  });
-  if (post) {
-    res.send(0);
+  if (!req.session.user) {
+    return res.send({ error: "You are not logged in." });
   } else {
-    res.send({ error: "Post not found." });
+    const post = await Post.find({
+      _id: req.params.postId,
+    });
+    if (post) {
+      res.send(0);
+    } else {
+      res.send({ error: "Post not found." });
+    }
   }
 });
 
 //Send post data upon editing
 app.get("/:postId", async (req, res) => {
-  try {
-    const post = await Post.find({
-      _id: req.params.postId,
-    });
-    if (!post) {
-      return res.send({ error: "Post not found." });
-    } else {
-      res.send({ post });
+  if (!req.session.user) {
+    return res.send({ error: "You are not logged in." });
+  } else {
+    try {
+      const post = await Post.find({
+        _id: req.params.postId,
+      });
+      if (!post) {
+        return res.send({ error: "Post not found." });
+      } else {
+        res.send({ post });
+      }
+    } catch (err) {
+      res.send({ error: "Invalid post" });
     }
-  } catch (err) {
-    res.send({ error: "Invalid post" });
   }
 });
 
 //Edit post data
 app.put("/:postId", async (req, res) => {
-  try {
-    const post = await Post.findOne({
-      _id: req.params.postId,
-      userId: req.session.user._id,
-    });
+  if (!req.session.user) {
+    return res.send({ error: "You are not logged in." });
+  } else {
+    try {
+      const post = await Post.findOne({
+        _id: req.params.postId,
+        userId: req.session.user._id,
+      });
 
-    if (!post) {
-      return res.send({ error: "Post is not found" });
-    } else {
-      post.title = req.body.title;
-      post.body = req.body.description;
-      await post.save();
-      res.send({ statusMsg: "Post edit is submitted" });
+      if (!post) {
+        return res.send({ error: "Post is not found" });
+      } else {
+        post.title = req.body.title;
+        post.body = req.body.description;
+        await post.save();
+        res.send({ statusMsg: "Post edit is submitted" });
+      }
+    } catch (err) {
+      console.log(err);
+      res.send({ error: "Something went wrong" });
     }
-  } catch (err) {
-    console.log(err);
-    res.send({ error: "Something went wrong" });
   }
 });
 
 //Send all votes to the client
 app.get("/:postId/votes", async (req, res) => {
-  try {
-    const post = await Post.findOne({
-      _id: req.params.postId,
-    });
-    if (!post) {
-      return res.send({ error: "Post not found." });
-    } else {
-      const votesUpCount = await PostVote.countDocuments({
-        post: req.params.postId,
-        vote: 1,
+  if (!req.session.user) {
+    return req.send({ error: "You are not logged in." });
+  } else {
+    try {
+      const post = await Post.findOne({
+        _id: req.params.postId,
       });
-      const votesDownCount = await PostVote.countDocuments({
-        post: req.params.postId,
-        vote: -1,
-      });
-      res.send({
-        votesUpCount,
-        votesDownCount,
-      });
+      if (!post) {
+        return res.send({ error: "Post not found." });
+      } else {
+        const votesUpCount = await PostVote.countDocuments({
+          post: req.params.postId,
+          vote: 1,
+        });
+        const votesDownCount = await PostVote.countDocuments({
+          post: req.params.postId,
+          vote: -1,
+        });
+        res.send({
+          votesUpCount,
+          votesDownCount,
+        });
+      }
+    } catch (err) {
+      console.log(err);
+      res.send({ error: "Something went wrong with the voting system." });
     }
-  } catch (err) {
-    console.log(err);
-    res.send({ error: "Something went wrong with the voting system." });
   }
 });
 
