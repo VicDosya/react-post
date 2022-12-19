f69a2d0b878c3e422fdbec69f0a2e02ba9e7de93
dosya <vicdosya@gmail.com>
2022-12-19 12:01:47 +0200

    Reverse post and comment lists.

--- client/src/Components/CommentList.js
+++ client/src/Components/CommentList.js
@@ -4,18 +4,20 @@ import Comment from "./Comment";
 function CommentList({ comments, postId, onDelete }) {
   return (
     <div>
-      {comments.map((comment, key) => (
-        <Comment
-          postId={postId}
-          commentId={comment._id}
-          userId={comment.userId}
-          body={comment.body}
-          author={comment.author}
-          commentDate={comment.createdAt}
-          onDelete={onDelete}
-          key={key}
-        ></Comment>
-      )).reverse()}
+      {comments
+        .map((comment, key) => (
+          <Comment
+            postId={postId}
+            commentId={comment._id}
+            userId={comment.userId}
+            body={comment.body}
+            author={comment.author}
+            commentDate={comment.createdAt}
+            onDelete={onDelete}
+            key={key}
+          ></Comment>
+        ))
+        .reverse()}
     </div>
   );
 }
